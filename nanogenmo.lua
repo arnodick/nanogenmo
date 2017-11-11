63d046cc6a46d234af544ddf565bb8b613fb0f2c
Ashley Pringle <ash.pringle@gmail.com>
2017-11-11 18:02:17 -0400

    Made a VERY large noun list

--- nanogenmo.lua
+++ nanogenmo.lua
@@ -2,15 +2,29 @@ LIP = require("LIP")
 supper = require("supper")
 nano = require("nano")
 
+local numberfiles = function(filename)
+	local s=""
+	local i=1
+	for line in io.lines(filename) do
+		s=s..i.."="..line.."\n"
+		i=i+1
+	end
+	return s
+end
+
 math.randomseed(os.time())
 
 --TODO this will be the generator seed state thing
 --it's where things like character names (main character, antagonist etc) go
 local g={}
 
-local f=io.open("nanogenmo.txt","a")
+--local f=io.open("nanogenmo.txt","a")
+local f=io.open("newnounlist.txt","w")
 
-nano.book(f,4)
+s=numberfiles("nounlist.txt")
+f:write(s)
+
+--nano.book(f,4)
 --nano.sentence.build(f)
 
 f:close()
