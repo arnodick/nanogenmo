d0cb4967bfbc7c1681837334dbdddce5547d7d84
Ashley Pringle <ash.pringle@gmail.com>
2017-11-16 23:12:18 -0400

    Whoops

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <BODY>
 	<p>GENERATING...</p>
 	<?php
-		$output = shell_exec('lua52 /home/public/luaprojects/nanogenmo/nanogenmo.lua blub borp');
+		//$output = shell_exec('lua52 /home/public/luaprojects/nanogenmo/nanogenmo.lua blub borp');
+		$output = shell_exec('lua52 /home/public/luaprojects/nanogenmo/n.lua');
 		if (is_null($output))
 		{
 			echo "no";
