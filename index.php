8a728aeb7a55eca05ed7e9afea477ca7f5c9d748
Ashley Pringle <ash.pringle@gmail.com>
2017-11-21 18:42:51 -0400

    Removed cgi file

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <HTML>
 <BODY>
+	<form>
+		Protagonist:<br>
+		<input type="text" name="protagonist"><br>
+		Antagonist:<br>
+		<input type="text" name="antagonist">
+	</form> 
 	<p>GENERATING...</p>
 	<?php
 		$output = shell_exec('lua52 nanogenmo.lua');
--- zindex.cgi
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/lua52
-io.write('Content-Type: text/plain\n\n', "Writing A Book!", '\n', '...', '\n')
-if arg[1] then
-	io.write(arg[1])
-else
-	io.wrote("nope")
-end
-require("nanogenmo")
-io.write('Done!', '\n')
