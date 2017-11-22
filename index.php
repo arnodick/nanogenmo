a6c89a48d039ecc9b57d123084796fadc59547e1
Ashley Pringle <ash.pringle@gmail.com>
2017-11-22 00:42:24 -0400

    this should be funnnnn

--- index.php
+++ index.php
@@ -10,10 +10,11 @@
 		{
 			$protagonist = escapeshellarg($protagonist);
 			$antagonist = escapeshellarg($antagonist);
-			echo $_SERVER["REMOTE_USER"];
 			$output = shell_exec("lua52 nanogenmo.lua ".$protagonist." ".$antagonist);
 			echo "Done!";
 			echo "<pre>$output</pre>";//TODO this is a hack why does this work only with this line?
+			$user = $_SERVER["REMOTE_USER"];
+			mail("ash.pringle@gmail.com", $user." used up their book generation tokens", "yeah you heard it right! ".$user." used up their book generation tokens");
 		}
 		else
 		{
