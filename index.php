981c1d00474fdd14610dc80db38df4c36e857932
Ashley Pringle <ash.pringle@gmail.com>
2017-11-22 00:15:49 -0400

    okay

--- index.php
+++ index.php
@@ -4,13 +4,12 @@
 	<p>GENERATING</p>
 	<p>...</p>
 	<p><?php
-		$h1 = hash("md5", $_POST["protagonist"]);
-		$protagonist = escapeshellarg($_POST["protagonist"]);
-		$h2 = hash("md5", $protagonist);
-		$antagonist = escapeshellarg($_POST["antagonist"]);
+		$protagonist = $_POST["protagonist"];
+		$antagonist = $_POST["antagonist"];
 		if ($protagonist and $antagonist)
 		{
-			echo $h1."\n".$h2."\n";
+			$protagonist = escapeshellarg($protagonist);
+			$antagonist = escapeshellarg($antagonist);
 			$output = shell_exec("lua52 nanogenmo.lua ".$protagonist." ".$antagonist);
 			echo "Done!";
 			echo "<pre>$output</pre>";//TODO this is a hack why does this work only with this line?
