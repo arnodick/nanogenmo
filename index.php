32364c9602c78c679df13e78f56a0a3c08a50c2d
Ashley Pringle <ash.pringle@gmail.com>
2017-11-21 22:17:42 -0400

    noooo iudea

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 		$antagonist = $_POST["antagonist"];
 		$output = shell_exec("lua52 nanogenmo.lua ".$protagonist." ".$antagonist);
 		echo "Done!";
+		echo "<pre>$output</pre>";
 	?></p>
 </BODY>
 </HTML>
