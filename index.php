50140345b6c023ac9c3619f07346eb8f326bdf6c
Ashley Pringle <ash.pringle@gmail.com>
2017-11-25 23:46:29 -0400

    um

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 			echo "Done!";
 			//echo "<pre>$output[0]</pre>";//TODO this is a hack why does this work only with this line?
 			echo $output[0];//TODO this is a hack why does this work only with this line?
-			var_dump($output);
+			//var_dump($output);
 			if ($user)
 			{
 				mail("ash.pringle@gmail.com", $user." used up their book generation tokens", "yeah you heard it right! ".$user." used up their book generation tokens");
