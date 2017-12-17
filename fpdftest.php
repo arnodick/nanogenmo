1cc13ddab07f22972a2d36a8ff2077e9032eb5e1
Ashley Pringle <ash.pringle@gmail.com>
2017-12-17 14:42:03 -0400

    frame?

--- fpdftest.php
+++ fpdftest.php
@@ -3,6 +3,6 @@
 	$pdf = new FPDF('L', 'mm', 'Legal');
 	$pdf->AddPage();
 	$pdf->SetFont('ZapfDingbats', 'B', 16);
-	$pdf->Cell(40, 10, 'What up Dawn');
+	$pdf->Cell(40, 10, 'What up Dawn', 1);
 	$pdf->Output();
 ?>
