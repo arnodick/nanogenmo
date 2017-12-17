8d5f6764e547d7989eacd7a5e4f693f17a78d0ae
Ashley Pringle <ash.pringle@gmail.com>
2017-12-17 15:59:30 -0400

    doin it

--- fpdftest.php
+++ fpdftest.php
@@ -1,9 +1,35 @@
 <?php
 	require('library/fpdf.php');
-	$pdf = new FPDF('L', 'mm', 'Legal');
+
+	class PDF extends FPDF// PDF INHERITS FROM FPDF AND OVERRIDES HEADER AND FOOTER FUNCTIONS WHICH ARE AUTOMATICALLY CALLED BY FPDF
+	{
+		function Header()// PAGE HEADER
+		{
+			//$this->Image('logo.png', 10, 6, 30);// LOGO
+			$this->SetFont('Arial', 'B', 15);// MOVE TO THE RIGHT
+			$this->Cell(80);// TITLE
+			$this->Cell(30, 10, 'Title', 1, 0, 'C');// LINE BREAK
+			$this->Ln(20);
+		}
+
+		function Footer()// PAGE FOOTER
+		{
+			$this->SetY(-15);// POSITION AT 15mm FROM BOTTOM
+			$this->SetFont('Arial', 'I', 8);
+			$this->Cell(0, 10, 'Page '.this->PageNo().'/{nb}', 0, 0 'C');// PAGE NUMBER
+		}
+	}
+
+
+	//$pdf = new FPDF('L', 'mm', 'Legal');
+	$pdf = new PDF():
+	$pdf->AliasNbPages();
 	$pdf->AddPage();
-	$pdf->SetFont('Arial', 'B', 16);
+	//$pdf->SetFont('Arial', 'B', 16);
+	$pdf->SetFont('Times', '', 12);
 	$pdf->Cell(40, 10, 'What up Dawn', 1);
 	$pdf->Cell(60, 10, 'The Story Of Dawn', 0, 1, 'C');
+	for($i=1; $i<=40; $i++)
+		$pdf->Cell(0, 10, 'Printing line number: '.$i, 0, 1);
 	$pdf->Output();
 ?>
