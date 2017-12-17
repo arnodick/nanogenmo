4f6b4bc3060c78622c01aadd92acc7f1426e941a
Ashley Pringle <ash.pringle@gmail.com>
2017-12-17 14:38:34 -0400

    DINGBATS

--- fpdftest.php
+++ fpdftest.php
@@ -2,7 +2,7 @@
 	require('library/fpdf.php');
 	$pdf = new FPDF('L', 'mm', 'Legal');
 	$pdf->AddPage();
-	$pdf->SetFont('Arial', 'B', 16);
+	$pdf->SetFont('ZapfDingbats', 'B', 16);
 	$pdf->Cell(40, 10, 'What up Dawn');
 	$pdf->Output();
 ?>
