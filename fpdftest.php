b08b5b0b70b9180815b0b8b324e93ff18df1204a
Ashley Pringle <ash.pringle@gmail.com>
2017-12-17 14:44:30 -0400

    line

--- fpdftest.php
+++ fpdftest.php
@@ -2,7 +2,8 @@
 	require('library/fpdf.php');
 	$pdf = new FPDF('L', 'mm', 'Legal');
 	$pdf->AddPage();
-	$pdf->SetFont('ZapfDingbats', 'B', 16);
+	$pdf->SetFont('Arial', 'B', 16);
 	$pdf->Cell(40, 10, 'What up Dawn', 1);
+	$pdf->Cell(60, 10, 'The Story Of Dawn', 0, 1, 'C');
 	$pdf->Output();
 ?>
