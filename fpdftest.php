404e88beb763fe5f3bf8918ae3ffb5f1397a9efb
Ashley Pringle <ash.pringle@gmail.com>
2017-12-17 14:33:38 -0400

    page size legal

--- fpdftest.php
+++ fpdftest.php
@@ -1,6 +1,6 @@
 <?php
 	require('library/fpdf.php');
-	$pdf = new FPDF();
+	$pdf = new FPDF('L', 'mm', 'Legal');
 	$pdf->AddPage();
 	$pdf->SetFont('Arial', 'B', 16);
 	$pdf->Cell(40, 10, 'What up Dawn');
